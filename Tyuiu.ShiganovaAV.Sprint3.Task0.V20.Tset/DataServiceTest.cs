438dacb7bb3152ce8539a75572a85741b2a3e1ea
Anastasia Shiganova <Tyuiu.ShiganovaAV@outlook.com>
2025-10-15 21:16:09 +0500

    Add project files.

---
+++ Tyuiu.ShiganovaAV.Sprint3.Task0.V20.Tset/DataServiceTest.cs
@@ -0,0 +1,19 @@
+﻿using Tyuiu.ShiganovaAV.Sprint3.Task0.V20.Lib;
+namespace Tyuiu.ShiganovaAV.Sprint3.Task0.V20.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidGetMultiplySeries()
+        {
+            DataService ds = new DataService();
+            int value = 1;
+            int startValue = 1;
+            int stopValue = 14;
+            double res = ds.GetMultiplySeries(value, startValue, stopValue);
+            double wait = 0;
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.ShiganovaAV.Sprint3.Task0.V20/Program.cs
@@ -0,0 +1,35 @@
+﻿using Tyuiu.ShiganovaAV.Sprint3.Task0.V20.Lib;
+namespace Tyuiu.ShiganovaAV.Sprint3.Task0.V20
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #1 |Выполнила: Шиганова А.В.| ПИНб-25-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #2                                                               *");
+            Console.WriteLine("* Тема: Алгоритмы разветвляющейся структуры                               *");
+            Console.WriteLine("* Задание #7                                                              *");
+            Console.WriteLine("* Вариант #5                                                              *");
+            Console.WriteLine("* Выполнила: Шиганова Анастасия Владимировна |ПИНб-25-1|                  *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+            Console.Write("Введите k: ");
+            int value = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Введите начальное число: ");
+            int startValue = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Введите последнее число: ");
+            int stopValue = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            double res = ds.GetMultiplySeries(value, startValue, stopValue);
+            Console.WriteLine(res);
+            Console.ReadKey();
+        }
+    }
+}
