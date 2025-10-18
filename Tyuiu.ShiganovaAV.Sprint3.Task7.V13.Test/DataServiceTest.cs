14d53bf0be9a1c300e5696ec2743dc1a2b3e7725
Anastasia Shiganova <Tyuiu.ShiganovaAV@outlook.com>
2025-10-18 21:51:33 +0500

    .

---
+++ Tyuiu.ShiganovaAV.Sprint3.Task7.V13.Test/DataServiceTest.cs
@@ -0,0 +1,37 @@
+﻿using Tyuiu.ShiganovaAV.Sprint3.Task7.V13.Lib;
+namespace Tyuiu.ShiganovaAV.Sprint3.Task7.V13.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+
+            int startValue = -5;
+            int stopValue = 5;
+            int len = stopValue - startValue + 1;
+
+            double[] valueWaitArray;
+            valueWaitArray = new double[len];
+
+            valueWaitArray[0] = -9.11;
+            valueWaitArray[1] = -6.84;
+            valueWaitArray[2] = -4.61;
+            valueWaitArray[3] = -2.42;
+            valueWaitArray[4] = -0.27;
+            valueWaitArray[5] = 0.00;
+            valueWaitArray[6] = 2.27;
+            valueWaitArray[7] = 4.50;
+            valueWaitArray[8] = 6.73; 
+            valueWaitArray[9] = 8.96;
+            valueWaitArray[10] = 11.19;
+
+            double[] res;
+            res = new double[len];
+            res = ds.GetMassFunction(startValue, stopValue);
+            CollectionAssert.AreEqual(valueWaitArray, res);
+        }
+    }
+}
---
+++ Tyuiu.ShiganovaAV.Sprint3.Task7.V13/Program.cs
@@ -0,0 +1,40 @@
+﻿using Tyuiu.ShiganovaAV.Sprint3.Task7.V13.Lib;
+namespace Tyuiu.ShiganovaAV.Sprint3.Task7.V13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #3 |Выполнила: Шиганова А.В.| ПИНб-25-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #3                                                               *");
+            Console.WriteLine("* Тема: Добавление к решению итоговых проектов по спринту                 *");
+            Console.WriteLine("* Задание #7                                                              *");
+            Console.WriteLine("* Вариант #13                                                             *");
+            Console.WriteLine("* Выполнила: Шиганова Анастасия Владимировна |ПИНб-25-1|                  *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+            int startValue = -5;
+            Console.Write("Начало: "+startValue);
+            int stopValue = 5;
+            Console.Write("Конец: "+stopValue);
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            double[] res = ds.GetMassFunction(startValue, stopValue);
+            int len = res.Length;
+
+            for (int i = 0; i < len; i++)
+            {
+                Console.WriteLine("X = {0,3} | F(x) = {1,7:F2}", startValue + i, res[i]);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
+
